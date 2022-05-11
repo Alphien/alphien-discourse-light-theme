0360a6f8ccae4f04da7bf84291bcea6bb2396e71
adi4ecap <65321548+adi4ecap@users.noreply.github.com>
2022-05-12 05:24:29 +0800

    Alphien blog (#1)
    * add style and js script for supporting blog
    
    * remove border radius on blog button
    
    * remove bg body
    
    * blog main page heading
    
    * update
    
    * update
    
    * update tag
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add more excerpt length
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update style
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update box shadow
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add width 100% on post content detail page
    
    * add padding bottom for blog main page
    
    * change svg fill
    
    * change to correct svg style fill
    
    * add sear box blog
    
    * add blog search form submit handle
    
    * make search button position absolute
    
    * remove style for blog topic list first child
    
    * update topic by line style
    
    * divide main blog page per sub category
    
    * change hover color on topic list item
    
    * add blog sub category style
    
    * add style for readmore category
    
    * add style for active header and tag link
    
    * add style for loading topic list
    
    * add style for blog post category link
    
    * fix pre tag auto x scroll and fix mobile menu active
    
    * arrange comments element order in blog post detail
    
    * change content font to PT Serif
    
    * edit meta style in topic by line
    
    * add button like style
    
    * add blog like function
    
    * fix correct PT Serif fonts url
    
    * update font size and font source blog post
    
    * change subscribe redirect and auto increment like value without refresh page
    
    * add tags widget margin
    
    * fix typo flag_topic param on blog post like API

---
+++ javascripts/blog.js
@@ -0,0 +1,104 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  var $blog = $("body.blog, body.blog-post, body.blog-sub-category");
+  var $subscribeModal = $blog.find(".modal.subscribe");
+  var $subscribeBtn = $blog.find(".btn-subscribe");
+  var $blogSearch = $blog.find("#blog-search");
+  var $blogLikeBtn = $blog.find(".btn-like").not('.liked');
+  var prod = window.location.host === "forum.alphien.com";
+
+  $blog.find($subscribeBtn).on("click", function(e) {
+    if ($(this).hasClass("user")) {
+      $subscribeModal.addClass("active");
+    } else {
+      if (prod) {
+        window.location.href = "https://auth.alphien.com/?s=" + encodeURIComponent(window.location.href);
+      } else {
+        window.location.href = "https://beta-auth.alphien.com/?s=" + encodeURIComponent(window.location.href);
+      }
+    }
+  });
+
+  $subscribeModal.find(".modal.subscribe .modal-close").on("click", function(e) {
+    $subscribeModal.removeClass("active");
+  });
+
+  $blog.on("click", function(e) {
+    if ($(e.target).closest(".modal.subscribe .modal-content, .btn-subscribe").length == 0) {
+      $subscribeModal.removeClass("active");
+    }
+  });
+
+  $blog.find(".canvas p").each(function() {
+    let $this = $(this);
+    if ($this.html().replace(/\s|&nbsp;/g, "").length == 0) {
+      $this.remove();
+    }
+  });
+
+  $blogSearch.submit(function(e){
+    e.preventDefault()
+    var searchValue = $blogSearch.find("input").val();
+    if (searchValue !== "") {
+      var url = new URL(location.href);
+      var search_params = url.searchParams;
+      search_params.set("q", searchValue);
+      url.search = search_params.toString();
+      location.href = url.toString();
+    }
+  });
+
+  $blog.find(".header-buttons a").each(function() {
+    var linkHeader = $(this).attr("href");
+    if (linkHeader && window.location.pathname.search(linkHeader) > -1 && linkHeader !== "/"){
+       $(this).addClass("active");
+    }
+  });
+
+  $blog.find(".tag-container .tag-list li a").each(function() {
+    var linkTag = $(this).attr("href").substring(1);
+    if (window.location.search.substring(1).search(linkTag) > -1){
+      $(this).addClass("active");
+    }
+  });
+
+  $blog.find($blogLikeBtn).on("click", function(e) {
+    if ($(this).hasClass("user")) {
+      const data = {
+        "id": $(this).attr("post-id"),
+        "post_action_type_id": 2,
+        "flag_topic": false
+      }
+      $.ajax({
+        type: "POST",
+        url: "/post_actions",
+        data,
+        headers: {
+          "X-CSRF-Token": $("meta[name=csrf-token]").attr("content")
+        },
+        returnXHR: true
+      })
+      .done(function() {
+        $blogLikeBtn.addClass("liked");
+        $blogLikeBtn.removeClass("user");
+        $blogLikeBtn.attr("title", "you already liked this post");
+        $blogLikeBtn.find("span").text("Liked");
+        $blog.find($blogLikeBtn).off('click');
+        const $likeValue = $blog.find(".like-meta-value");
+        if ($likeValue) {
+          const currentValue = parseInt($likeValue.text());
+          const newValue = currentValue + 1;
+          $likeValue.text(newValue);
+        }
+      })
+      .fail(function() {
+        $(".comment-button").after("<div style='color: #dd1144; margin-top: 5px;'>failed to like post</div>")
+      })
+    } else {
+      if (prod) {
+        window.location.href = "https://auth.alphien.com/?s=" + encodeURIComponent(window.location.href)
+      } else {
+        window.location.href = "https://beta-auth.alphien.com/?s=" + encodeURIComponent(window.location.href)
+      }
+    }
+  });
+});
